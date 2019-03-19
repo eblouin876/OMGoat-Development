bfc474e1774bc95c2409f711cc9609e17b0cdc9e
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-03-19 11:29:29 -0700

    Changed hangman image

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -74,7 +74,10 @@ class Portfolio {
 
 }
 
-const hangman = new PortfolioCard("Hangman", "A basic hangman game built from HTML, CSS, and JavaScript", "assets/images/Hangman.jpg", "https://eblouin876.github.io/Word-Guess-Game/")
+// TODO: Add modal to card build that appears on hover over the image itself.
+// Should bring focus to modal and away from background
+
+const hangman = new PortfolioCard("Hangman", "A basic hangman game built from HTML, CSS, and JavaScript", "assets/images/hangman.png", "https://eblouin876.github.io/Word-Guess-Game/")
 const starWarsRPG = new PortfolioCard("Star Wars RPG", "Basic Star Wars RPG built with HTML, CSS, JavaScript, and JQuery", "assets/images/star-wars.jpg", "https://eblouin876.github.io/unit-4-game/")
 const Trivia = new PortfolioCard("Trivia Game", "A basic trivia game with multiple categories of questions to choose from built off of an open source trivia API", "assets/images/trivia.jpg", "https://eblouin876.github.io/TriviaGame/")
 const GifTastic = new PortfolioCard("Gif Tastic", "A simple page where you can go to generate random Gifs by subject, play and puase, and choose to download", "assets/images/gif.gif", "https://eblouin876.github.io/Gif-Tastic/")
