99087c46675150f2b4af9fdf9c0663cf3261ce3f
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-03-24 17:06:47 -0700

    Changed TTT URL

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -84,7 +84,7 @@ const barkParkFinder = new PortfolioCard("Bark Park Finder", "A web application 
 const starWarsRPG = new PortfolioCard("Star Wars RPG", "Basic Star Wars RPG built with HTML, CSS, JavaScript, and JQuery", "assets/images/star-wars.jpg", "https://eblouin876.github.io/unit-4-game/")
 const Trivia = new PortfolioCard("Trivia Game", "A basic trivia game with multiple categories of questions to choose from built off of an open source trivia API", "assets/images/trivia.jpg", "https://eblouin876.github.io/TriviaGame/")
 const GifTastic = new PortfolioCard("Gif Tastic", "A simple page where you can go to generate random Gifs by subject, play and puase, and choose to download", "assets/images/gif.gif", "https://eblouin876.github.io/Gif-Tastic/")
-const TTTMultiplayer = new PortfolioCard("Tic Tact Toe", "A multiplayer tic tac toe game that allows users to challenge anyone actively on the site. Built on firebase", "assets/images/ttt.jpg", "https://eblouin876.github.io/RPS-Multiplayer/")
+const TTTMultiplayer = new PortfolioCard("Tic Tact Toe", "A multiplayer tic tac toe game that allows users to challenge anyone actively on the site. Built on firebase", "assets/images/ttt.jpg", "https://eblouin876.github.io/TTT-Multiplayer/")
 let port = [barkParkFinder, starWarsRPG, Trivia, GifTastic, TTTMultiplayer]
 
 
